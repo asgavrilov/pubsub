b5fc11e194981806b3cdbdf235a6142f0d74b2bc
asgavrilov <alexander.s.gavrilov@yandex.com>
2022-07-21 17:44:02 +0300

    adds with queue impl

--- src/broker/ConsumerQueue.java
+++ src/broker/ConsumerQueue.java
@@ -3,20 +3,30 @@ package broker;
 import consumer.IConsumerCallback;
 import pojo.Message;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
-public class ConsumerQueue{
-    private final Executor consumerExecutor = Executors.newSingleThreadExecutor();
+public class ConsumerQueue implements Runnable {
     private final IConsumerCallback callback;
+
+    private final BlockingQueue<Message> queue = new LinkedBlockingQueue<>();
 
     public ConsumerQueue(IConsumerCallback callback) {
         this.callback = callback;
     }
 
     public void send(Message message) {
-        consumerExecutor.execute(() -> {
-            this.callback.run(message);
-        });
+        this.queue.add(message);
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                this.callback.run(queue.take());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
--- src/broker/MessageBroker.java
+++ src/broker/MessageBroker.java
@@ -5,29 +5,30 @@ import pojo.Message;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MessageBroker implements IMessageBroker {
-    private final Map<String, List<ConsumerQueue>> consumersTopicMap = new ConcurrentHashMap<>();
+    private final Executor executor = Executors.newCachedThreadPool();
+    private final ConcurrentHashMap<String, List<ConsumerQueue>> consumersTopicMap = new ConcurrentHashMap<>();
 
 
     @Override
     public void register(String topic, IConsumerCallback consumer) {
         ConsumerQueue consumerQueue = new ConsumerQueue(consumer);
-        consumersTopicMap.compute(topic, (s, t) -> {
-            if (t == null) {
-                List<ConsumerQueue> list = new ArrayList<>();
-                list.add(consumerQueue);
-                return list;
-            } else {
-                t.add(consumerQueue);
-                return t;
-            }
-        });
+        executor.execute(consumerQueue);
+        if (consumersTopicMap.containsKey(topic)) {
+            consumersTopicMap.get(topic).add(consumerQueue);
+        } else {
+            ArrayList<ConsumerQueue> list = new ArrayList<>();
+            list.add(consumerQueue);
+            consumersTopicMap.put(topic, list);
+        }
     }
 
     @Override
     public void send(Message message) {
-        consumersTopicMap.getOrDefault(message.getTopic(),List.of()).
+        consumersTopicMap.getOrDefault(message.topic, List.of()).
                 forEach(consumerQueue -> consumerQueue.send(message));
     }
 }
